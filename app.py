1ebe27fe6179afb4f5edc6507bea1f2fdb533343
Max Scheiber <keasbeynumb@gmail.com>
2013-09-07 10:46:06 -0400

    switch from polling to web socket

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_heroku import Heroku
 # Python library imports
 import oauth2 as oauth
 import os
+import pusher
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 import urllib
@@ -12,8 +13,10 @@ import urllib
 # Flask overhead
 app = Flask(__name__)
 heroku = Heroku(app)
-add_cache = []
-skip = False
+
+# Pusher overhead
+frontend = pusher.Pusher(app_id=os.environ['PUSHER_ID'],
+	key=os.environ['PUSHER_KEY'], secret=os.environ['PUSHER_SECRET'])
 
 ####################
 # HELPER FUNCTIONS #
@@ -36,22 +39,12 @@ def send_text(to, body):
 def home():
 	return render_template('index.html')
 
-@app.route('/poll', methods=['GET'])
-def poll():
-	global skip
-	res = {'add': [], 'skip': skip}
-	for x in add_cache:
-		res['add'].append(x)
-	add_cache[:] = []
-	skip = False
-	return json.dumps(res)
-
 # we need the currently playing song
 def queue_song(person, query):
 	# search Rdio for song
 	song_result = rdio({'method':'search', 'query':query, 'types':'Track', 'count':1})
 	song = json.loads(song_result[1])['result']['results'][0]
-	add_cache.append(song['key'])
+	frontend['juke'].trigger('queue', {'song':song['key']})
 
 	# text user confirmation
 	send_text(person, song['name'] + ' is queued, thank you!')
@@ -61,9 +54,8 @@ def queue_song(person, query):
 def twilio():
 	from_ = request.values.get('From', None)
 	msg = request.values.get('Body', None)
-	global skip
-	if msg.lower() == 'skip':
-		skip = True
+	if msg.lower() == 'skip' and from_[1:] == os.environ['TWILIO_ADMIN'][1:]:
+		frontend['juke'].trigger('skip')
 	else:
 		# right now, assuming all messages are the song name to play
 		queue_song(from_, msg)
@@ -89,8 +81,8 @@ twilio = TwilioRestClient(os.environ['TWILIO_KEY'], os.environ['TWILIO_SECRET'])
 
 # Flask overhead
 if __name__ == '__main__':
-	playback_token = json.loads(rdio({'method':'getPlaybackToken', 'domain':'deejay-pennapps.herokuapp.com'})[1])['result']
-	print playback_token
+	#playback_token = json.loads(rdio({'method':'getPlaybackToken', 'domain':'deejay-pennapps.herokuapp.com'})[1])['result']
+	#print playback_token
 	# create playlist if it does not already exist
 	app.run(use_reloader=False, debug=True)
 
